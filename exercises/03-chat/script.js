ce5ba7ac0ac45ddea13c53a448103bb4de413a56
Mads Studsgaard Rasmussen <madsrasmussendev@gmail.com>
2026-02-03 22:23:47 +0100

    feat: add 03-chat

---
+++ exercises/03-chat/script.js
@@ -0,0 +1,6 @@
+// Her kan du skrive din kode, du har adgang til objektet AI i denne fil.
+
+const chatInput = document.querySelector("#chat-input");
+const chatSendBtn = document.querySelector("#chat-send-btn");
+
+console.log(chatInput, chatSendBtn);
